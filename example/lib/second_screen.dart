2d0b76cd4705e8ef407abfbeafa0c6707d1f9ac1
MoveoOne Integration <integration@moveo.one>
2025-04-07 15:42:46 +0200

    MoveoOne automatic integration.

--- example/lib/second_screen.dart
+++ example/lib/second_screen.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:moveoone_flutter/moveoone_flutter.dart';
+import 'package:moveoone_flutter/src/constants.dart';
+import 'package:moveoone_flutter/src/moveo_one_data.dart';
 
 class SecondScreen extends StatelessWidget {
   const SecondScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    MoveoOne().track(
+      "second_screen",
+      MoveoOneData(
+        semanticGroup: "second_screen",
+        id: "second_screen_text",
+        type: MoveoOneType.text,
+        action: MoveoOneAction.view,
+        value: "Second Screen",
+        metadata: {"fontSize": "24"},
+      ),
+    );
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Second Screen'),
